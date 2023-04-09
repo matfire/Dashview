d5f10f5888349d963b56b8d590d8f380a88cbfc5
Matteo Gassend <mgassend@gmail.com>
2023-04-09 20:36:24 +0200

    add: enforce strict spacing

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,7 @@
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
   ignorePatterns: ["**/*.spec.ts", "**/*.spec.tsx"],
+  rules: {
+    indent: ["error", 2],
+  },
 };
