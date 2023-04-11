5b10779bb18bd24d2db0d3a0bc9cda1b994d6dda
Matteo Gassend <mgassend@gmail.com>
2023-04-11 15:24:37 +0200

    add: settings link in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,14 @@ export default function Header() {
   return (
     <header>
       <div className="navbar bg-base-100">
-        <button className="btn btn-ghost normal-case text-xl">DashView</button>
+        <div className="flex-1">
+          <a href="/" className="btn btn-ghost normal-case text-xl">DashView</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><a href="/settings">Settings</a></li>
+          </ul>
+        </div>
       </div>
     </header>
   );
