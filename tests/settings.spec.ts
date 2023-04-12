13de279b337c7f36547e1a4eaf330a5742161b4e
Matteo Gassend <mgassend@gmail.com>
2023-04-12 09:12:00 +0200

    fix: cookie test

--- tests/settings.spec.ts
+++ tests/settings.spec.ts
@@ -1,20 +1,18 @@
 import test, { expect } from "@playwright/test";
 
+test("default theme should be dark", async ({ page }) => {
+  await page.goto("/");
+  const body = await page.locator("body");
+  const themeColor = await body.getAttribute("data-theme");
+  expect(themeColor).toBe("dark");
+});
 
-test("default theme should be dark", async({page}) => {
-    await page.goto("/")
-    const body = await page.locator("body")
-    const themeColor = await body.getAttribute("data-theme");
-    expect(themeColor).toBe("dark")
-})
-test("changing the theme should change it", async({page}) => {
-    await page.goto("/settings");
-    const body = await page.locator("body")
-    const select = await page.locator("#themeColor");
-    const submitButton = await page.locator("button[type=\"submit\"]")
-    await select.selectOption(["light"])
-    await submitButton.click()
-    await page.goto("/")
-    const themeColor = await body.getAttribute("data-theme");
-    expect(themeColor).toBe("light")
-})
+test("changing the theme should change it", async ({ page }) => {
+  await page.goto("/settings");
+  const select = page.locator("#themeColor");
+  const submitButton = page.locator('button[type="submit"]');
+  await select.selectOption(["light"]);
+  await submitButton.click();
+  const body = page.locator("body");
+  await expect(body).toHaveAttribute("data-theme", "light");
+});
