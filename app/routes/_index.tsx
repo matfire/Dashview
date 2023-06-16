63cd9ee71e5831f6d8c95dd3608dbee10755055a
matfire <mgassend@gmail.com>
2023-06-16 16:44:51 +0200

    Feature/error handling (#9)
    * feature: Docker
    
    * Feature/actions (#2)
    
    * fix: removed .github folder from gitignore
    
    * add: workflows for test and deploy
    
    * fix: add lint step and update eslint
    
    * fix: linting
    
    * fix: linting test
    
    * add: enforce strict spacing
    
    * add: lint step in test job
    
    * fix: linting, again
    
    * Feature/settings (#3)
    
    * add: settings link in header
    
    * add: settings route
    
    * add: themes list from daisyui
    
    * add: cookie preferences
    
    * add: themeColor cookie is verified in root component
    
    * add: tests
    
    * fix: cookie test
    
    * fix: lint
    
    * fix: lint only app folder
    
    * add: README and license
    
    * Fix/color (#4)
    
    fix: text color is readable when theme is light color (#4)
    
    * Fix/image push on release
    
    * add: list of errors wip
    
    * fix: lint

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,7 +19,7 @@ export const loader: LoaderFunction = async () => {
         message: "could not parse the config file correctly",
         error: config.error,
       },
-      { status: 500 }
+      { status: 400 }
     );
   }
   return json(config.data);
@@ -45,6 +45,20 @@ export default function Index() {
     );
   }
   if (data.error) {
-    <p>something happened, check console</p>;
+    return <div>
+      <div className="alert alert-error shadow-lg">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>{data.message}</span>
+        </div>
+      </div>
+      <ul>
+        {data.error.issues.map((e) => (
+          <li key={e.path.join()} className="p-4">
+            <p>{e.message} at {e.path.join("=>")}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   }
 }
