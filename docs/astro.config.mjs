e604383166751a8b77d509b198b17bf319cadbec
matfire <mgassend@gmail.com>
2023-07-05 11:28:02 +0200

    Feature/docs (#10)
    * add: documentation website
    
    * fix: updated dockerignore

---
+++ docs/astro.config.mjs
@@ -0,0 +1,40 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Dashview",
+      social: {
+        github: "https://github.com/matfire/dashview",
+        discord: "https://discord.gg/RhDepYbx",
+        mastodon: "https://hachyderm.io/@mgassend"
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Getting Started",
+              link: "/guides/getting-started/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+  ],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
+});
---
+++ docs/src/content/docs/reference/app.md
@@ -0,0 +1,16 @@
+---
+title: "App"
+---
+
+## Definition
+
+An application is a service you want to keep track of
+
+## Structure
+
+| Name     | Type   | Required                        | Description                              |
+| -------- | ------ | ------------------------------- | ---------------------------------------- |
+| category | String | Yes (if categories are defined) | Defines what category the app belongs to |
+| name     | String | Yes                             | What the name of the app is              |
+| icon     | TDB    | No                              | Icon associated to the app               |
+| url      | String | Yes                             | The url for the app                      |
